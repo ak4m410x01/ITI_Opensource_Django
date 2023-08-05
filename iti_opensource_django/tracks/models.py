82584938b418f5780f318067081a74ea9af7d772
ak4m410x01 <abdallah10kamal@gmail.com>
2023-08-06 01:19:55 +0300

    define __str__ for Models

--- iti_opensource_django/tracks/models.py
+++ iti_opensource_django/tracks/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Track(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
     track = models.ForeignKey(Track, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
