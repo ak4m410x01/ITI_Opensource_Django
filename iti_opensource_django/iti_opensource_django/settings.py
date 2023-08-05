7d6f9ac2c77b02fda0e30c27139d143762478017
ak4m410x01 <abdallah10kamal@gmail.com>
2023-08-05 04:06:45 +0300

    add mysql-db connection config

--- iti_opensource_django/iti_opensource_django/settings.py
+++ iti_opensource_django/iti_opensource_django/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "iti_opensource_django.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "iti_opensource_django",
+        "USER": "ak4m410x01",
+        "PASSWORD": "ak4m410x01",
+        "HOST": "mysql-db",
     }
 }
 
