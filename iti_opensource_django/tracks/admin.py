ff1bd51435178bde34ccfa4f2495a00552bb022e
ak4m410x01 <abdallah10kamal@gmail.com>
2023-08-06 03:38:19 +0300

    CustomStudent Model Admin

--- iti_opensource_django/tracks/admin.py
+++ iti_opensource_django/tracks/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from .models import Track, Student
 
+
+class CustomStudent(admin.ModelAdmin):
+    fieldsets = (
+        [
+            "Student Information",
+            {
+                "fields": [
+                    "first_name",
+                    "last_name",
+                    "age",
+                ],
+            },
+        ],
+        [
+            "Scholarship Information",
+            {
+                "fields": [
+                    "track",
+                ],
+            },
+        ],
+    )
+
+
 # Register your models here.
 
 admin.site.register(Track)
-admin.site.register(Student)
+admin.site.register(Student, CustomStudent)
